bc64df1afd541e66f1e9bb823afac184b0e2e992
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-23 05:09:32 +0530

    add question 3

--- functions/question2.py
+++ functions/question2.py
@@ -4,8 +4,8 @@
 def num_fact(num):
     fact=1
     for i in range(1,len(list)+1):
-        t=total+list[i]
+        total=total+list[i]
     return total
 list=eval(input("Enter the list with numbers seperated by commas :"))
-mean=num_mean(list)/len(list)
+mean=num_fact(list)/len(list)
 print("mean of the elements of the list is :",mean)
