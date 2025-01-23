e60a1b6093150b3a278257dec0e1f48458a78a34
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-24 01:05:44 +0530

    add module intro

---
+++ functions/module.py
@@ -0,0 +1,24 @@
+#built in modules
+#math
+import math
+import random
+import statistics
+
+math.ceil(-9.7)
+math.floor(6.2)
+math.fabs(-4)#absolute value of x
+math.factorial(3)
+math.fmod(3,2)
+math.gcd(3,2)
+math.pow(3.2,2.2)
+math.sqrt(3.4)
+math.sin(2)
+
+random.random()#produces value btw 0 and 1
+random.randint(2,4)#2 and 4 are included
+random.randrange(2)#range from 0 to 2(excluding 2)
+random.randrange(2,7)
+
+statistics.mean([1,2,3])
+statistics.median([1,2,3])
+statistics.mode([1,2,2,3])
