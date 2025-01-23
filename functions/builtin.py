06b3ed0a1016e1b825e76c32b2265290390efd93
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-23 21:44:59 +0530

    add built in function intro

---
+++ functions/builtin.py
@@ -0,0 +1,25 @@
+#built in functions
+
+#1) print function
+m=print("hello")
+print(m)
+
+#2)int function
+m=int(3.2)
+print(m)
+m=int()
+print(m)
+
+#3)input function
+m=input("Enter")
+print(type(m))
+
+#absolute-abs(x)
+#returns ositive value of integer or float
+abs(-3.4)
+
+#divmod(x,y)=(quotient, remainder)
+divmod(7,2)
+
+#max, min
+#pow(x,y,z)=(x^y)%z
