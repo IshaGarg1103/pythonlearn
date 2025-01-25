01e2f81e1f2a4f5065421c24975aa574170d76ad
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-26 03:44:51 +0530

    intro to strings

---
+++ strings/intro.py
@@ -0,0 +1,12 @@
+#strings
+s='hello'
+w="world"
+k='''how are you doing
+            -unknown.''' #double triple quotes can also be used
+#slicing in strings
+s='Hello world'
+s[1:5] #'ello'
+s[0:0] #''
+s[-1:] #'d'
+s[::-1] #reversing a string
+s[::-2] #drWolH
