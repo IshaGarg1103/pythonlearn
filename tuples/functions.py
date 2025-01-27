57ecd626baa398c33b458ccd04c29a2c41242188
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 03:07:26 +0530

    added more tuple functions

--- tuples/functions.py
+++ tuples/functions.py
@@ -9,3 +9,12 @@ tuple1 #('a', 'e', 'i', 'o', 'u')
 #count()
 tuple1 = (10,20,30,10,40,10,50)
 tuple1.count(90) #0
+
+#index()
+tuple1=(10,20,30,30)
+tuple1.index(30) #2
+tuple1.index(3)  #error : x is not in tuple
+
+#sorted() : sorts the list in ascending order
+#max, min,sum
+#sum can't be operated for tuple with string data type
