24b891cee45a8dbf1cb6b05a12064cdd242ff5c6
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 02:56:55 +0530

    functions in tuple

---
+++ tuples/functions.py
@@ -0,0 +1,11 @@
+#functions in Tuple
+
+#len(tuple1)
+
+#tuple() #tuple function takes only one function
+tuple1=tuple('aeiou')
+tuple1 #('a', 'e', 'i', 'o', 'u')
+
+#count()
+tuple1 = (10,20,30,10,40,10,50)
+tuple1.count(90) #0
