8173e6d402170612ab8bdd9c9c1341a39be3ed65
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-27 23:53:37 +0530

    accessing elements in a tuple

--- tuples/introduction.py
+++ tuples/introduction.py
@@ -3,3 +3,19 @@
 tuple1=(1,2,3,4,5)#of same type
 tuple2=('economics',87,'accountancy',89.6) #of different data type
 tuple3=(1,2,3,[4,5])
+
+#wrong way to assign a single element
+tuple5=(20)
+type(tuple5) #integer
+#right way
+tuple5=(20,)
+type(tuple5) #tuple
+
+#a sequence without parentheses is treated as
+#tuple by default
+seq=1,2,3
+type(seq) #tuple
+
+#ACCESSING ELEMENTS IN A TUPLE
+tuple1=(2,4,6,8,10,12)
+tuple1[15] #error: index out of range
