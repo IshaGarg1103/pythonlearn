d47e578ad8f933284942816a1b2aea1bdd16ec4b
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 04:47:07 +0530

    intro to dictionary

---
+++ dictionary/intro.py
@@ -0,0 +1,12 @@
+#DICTIONARY
+#introduction
+dict1=dict()
+print(dict1) #{}
+
+#accessing items in a dictionary
+dict3={'jim':1,'karen':2,'pam':3}
+print(dict3['jim']) #1
+
+#dictionaries are mutable
+dict3['pam']=8
+#{'jim':1,'karen':2,'pam':8}
