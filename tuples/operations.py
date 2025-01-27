4228ebb07583cb4053f9aa651b4b10ab95f4ea32
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 02:49:07 +0530

    operations on tuple

--- tuples/operations.py
+++ tuples/operations.py
@@ -9,3 +9,13 @@ tuple1 + tuple2 #(1,3,5,7,9,2)
 tuple1=('hello','world')
 tuple1*3
 #('hello','world','hello','world','hello','world')
+
+#membership
+t=(1,2,4)
+2 in t #true
+
+#slicing
+tuple1=(10,20,30,40,50,60,70,80,90)
+tuple1[-4:-6] #()
+#reversed
+tuple1[::-1]
