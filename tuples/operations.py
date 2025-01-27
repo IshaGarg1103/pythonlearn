d7b451a2204a21ba0f296e99b65691e34d669e81
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 02:05:35 +0530

    operations on tuple

---
+++ tuples/operations.py
@@ -0,0 +1,11 @@
+#tuples operations
+#concatenation
+
+tuple1=(1,3,5,7,9)
+tuple2=(2,)
+tuple1 + tuple2 #(1,3,5,7,9,2)
+
+#repetition
+tuple1=('hello','world')
+tuple1*3
+#('hello','world','hello','world','hello','world')
