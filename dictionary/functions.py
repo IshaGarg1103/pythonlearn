c2bf000aaf9d8064fa6b8ec6ce0ac05f08fb0fee
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 23:45:05 +0530

    functions

--- dictionary/functions.py
+++ dictionary/functions.py
@@ -12,4 +12,14 @@ print(dict2) #{1: 'a', 2: 'b'}
 
 #keys
 dict1={1:'a',2:'b'}
-dict1.keys() #dict_keys([1, 2])
+dict1.keys() #dict_keys([1, 2]) #keys() take no argument
+
+#values : returns a list of values in the dictionary
+list1=list(dict1.values())
+print(list1) #['a','b']
+
+#items() : returns a list of tuples
+tuple=()
+tuple=list(dict1.items())
+tuple[0][1]  #'a'
+
