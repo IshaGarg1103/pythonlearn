f285bab6f850075d0efb95600c50ae487aa64b6c
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 20:01:22 +0530

    add operations on dictionary

---
+++ dictionary/operations.py
@@ -0,0 +1,20 @@
+#DICTIONARY OPERATIONS
+
+#membership
+dict1={'jim':1,'pam':2}
+print('karen' in dict1) #false
+
+#traversing a dictionary
+#method 1
+dict1={'jim':1,'pam':2}
+for key in dict1:
+    print(key,":",dict1[key])
+
+#method 2
+for key, value in dict1.items():
+    print(key,':',value)
+#-how does this work
+dict1.items() #dict_items([(1, 'jim'), (2, 'pam')])
+[key,item]=dict.items() #accessing the item and keys
+list=[key,item]
+list[0][0] #1
