bc82f26876f16b14651b7f5c06d40a9f3aaf1b62
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-28 21:27:05 +0530

    built in function in dictionary

---
+++ dictionary/functions.py
@@ -0,0 +1,11 @@
+#BUILT-IN FUNCTIONS
+
+#length of the dictionary
+dict1={}
+len(dict1)
+
+#converting list to dictionaries
+list1=[(1,'a'),(2,'b')] #another form :list1=[[1,'a'],[2,'b']]
+#tuple=((1,'a'),(2,'b')) #for tuple
+dict2=dict(list1)
+print(dict2) #{1: 'a', 2: 'b'}
