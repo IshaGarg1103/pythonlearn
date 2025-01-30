00c6c4d225af7ea8ee198a320d546068b732dc7b
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-01-30 23:53:22 +0530

    data type

---
+++ datatype.py
@@ -0,0 +1,8 @@
+#mutable and immutable data types
+#mutable : variables whose values can be changed after they are created
+#          and assigned are called mutable.
+#lists, sets, dictionary
+
+#immutable : whose value cannot be changed after they are created
+#            and assigned are called immutable.
+#integers, float, boolean, complex, strings, tuples
