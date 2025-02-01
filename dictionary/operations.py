1c290c7bf804254491d0ff1078abf46e90114527
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-01 16:38:34 +0530

    pushed all the files again

--- dictionary/operations.py
+++ dictionary/operations.py
@@ -15,6 +15,6 @@ for key, value in dict1.items():
     print(key,':',value)
 #-how does this work
 dict1.items() #dict_items([(1, 'jim'), (2, 'pam')])
-[key,item]=dict.items() #accessing the item and keys
+[key,item]=dict1.items() #accessing the item and keys
 list=[key,item]
 list[0][0] #1
