f4a67c69b9764445e3ecbc5d6a753054e112f0f9
IshaGarg1103 <ishagarg.1103@gmail.com>
2025-02-08 21:15:39 +0530

    executio of inheritance code

---
+++ inheritance/chefexe.py
@@ -0,0 +1,5 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+chef1 = ChineseChef() #make an instance by calling the class
+chef1.make_special_dish() #call the make_chicken() on the instance
